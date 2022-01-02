0781d1644bdcebaaa8ecbf0e47fca9bfce18e734
godhye <exmr12@gmail.com>
2022-01-02 19:58:00 +0900

    아직 완료되자말자 가져가진않음;

--- async/promise_future.cpp
+++ async/promise_future.cpp
@@ -9,33 +9,44 @@ using std::string;
 bool done = false;
 std::condition_variable cv;
 std::mutex mu;
+int ncount = 0;
 int nmessage;
 std::vector<std::thread> vectorthread;
+std::vector<std::thread> consumethread;
 
 void work(int a)
 {
 	{
 		std::lock_guard<std::mutex> lock(mu);
 		nmessage = a;
-		printf("nmessage =  %d\n", nmessage);
+		ncount++;
+		printf("work =  %d ncount = %d \n", nmessage, ncount);
 
 	}
+	if (ncount == 3)
+		done = true;
 
 	cv.notify_one();
-	if (a == 2)
-		done = true;
+
 }
 
 void consume() {
 
+	while (1)
+	{
+		std::unique_lock<std::mutex> lock(mu);
+		// done이 false이면  wait
+		cv.wait(lock, [&] {return done; });
 
-	std::unique_lock<std::mutex> lock(mu);
-	cv.wait(lock, [] {return done; });
+		if (ncount == 3)
+			printf("consume =  %d , nCount =  %d\n", nmessage, ncount);
+		lock.unlock();
+		return;
 
-
-	printf("consume =  %d\n", nmessage);
-	lock.unlock();
-
+		printf("consume =  %d , nCount =  %d\n", nmessage, ncount);
+		lock.unlock();
+		std::this_thread::sleep_for(std::chrono::microseconds(50));
+	}
 
 
 
@@ -50,20 +61,24 @@ int main() {
 
 	vectorthread.reserve(3);
 	for (int i = 0; i < 3; i++)
-		vectorthread.emplace_back([]() {consume(); });
-
-
-	for (int i = 0; i < 3; i++)
 	{
-		work(i);
-		_sleep(3009);
-	}
 
+		vectorthread.emplace_back([&]() {work(i); });
+		consumethread.emplace_back([]() {consume(); });
+	}
 
 	for (int i = 0; i < 3; i++)
 	{
 		vectorthread[i].join();
 	}
+	cv.notify_all();
 	printf("end\n");
+
+	for (int i = 0; i < 3; i++)
+	{
+
+		consumethread[i].join();
+	}
+
 	return 0;
 }
