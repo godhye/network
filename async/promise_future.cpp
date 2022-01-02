81c1d601a3e4421e39b23508ee5a4535f981742c
godhye <exmr12@gmail.com>
2022-01-02 17:04:50 +0900

    로직 수정 근데 안돌아

--- async/promise_future.cpp
+++ async/promise_future.cpp
@@ -18,45 +18,50 @@ void work(int a)
 		std::lock_guard<std::mutex> lock(mu);
 		nmessage = a;
 
-		if (a == 2)
-			done = true;
+
 	}
 
 	cv.notify_one();
+	if (a == 2)
+		done = true;
+}
+
+void consume() {
+
+	std::unique_lock<std::mutex> lock(mu);
+	cv.wait(lock, [] {return done; });
+
+	if (done)
+		return;
+
+	printf("work =  %d\n", nmessage);
+	lock.unlock();
+
 
 }
+
+
 
 
 int main() {
 
 
-	//int값을 반환할 예정인 promise(생산자역할)
-	std::promise<int> p;
-	//int값 반환(소비자역할)
-	std::future<int> f;
-
-	//미래에 int값 데이터 돌려주겟다.
-	f = p.get_future();
-
 	vectorthread.reserve(3);
 	for (int i = 0; i < 3; i++)
-		vectorthread.emplace_back(std::bind(work, i));
+		vectorthread.emplace_back(consume);
 
-	while (1)
+
+	for (int i = 0; i < 3; i++)
 	{
-		std::unique_lock<std::mutex> lock(mu);
-		cv.wait(lock, [] {return done; });
+		work(i);
 
-		if (done)
-			return;
-
-		printf("work =  %d\n", nmessage);
-		lock.unlock();
-		for (int i = 0; i < 3; i++)
-			vectorthread[i].join();
 	}
 
 
+	for (int i = 0; i < 3; i++)
+	{
+		vectorthread[i].join();
+	}
 
 	return 0;
 }
