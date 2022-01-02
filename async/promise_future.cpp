a075bc006845730ec3beea0f95d33bfd86aa7797
godhye <exmr12@gmail.com>
2022-01-02 21:27:35 +0900

    conditional_variable wait담에괄호 !!!!! ㅂㄷㅂㄷ

--- async/promise_future.cpp
+++ async/promise_future.cpp
@@ -3,14 +3,14 @@
 #include <string>
 #include <thread>
 #include <mutex>
-
+#include <queue>
 using std::string;
 
 bool done = false;
 std::condition_variable cv;
 std::mutex mu;
 int ncount = 0;
-int nmessage;
+std::queue<int> jobq;
 std::vector<std::thread> vectorthread;
 std::vector<std::thread> consumethread;
 
@@ -18,13 +18,12 @@ void work(int a)
 {
 	{
 		std::lock_guard<std::mutex> lock(mu);
-		nmessage = a;
+		jobq.push(a);
 		ncount++;
-		printf("work =  %d ncount = %d \n", nmessage, ncount);
+		printf("work =  %d ncount = %d \n", a, ncount);
 
 	}
-	if (ncount == 3)
-		done = true;
+
 
 	cv.notify_one();
 
@@ -35,15 +34,17 @@ void consume() {
 	while (1)
 	{
 		std::unique_lock<std::mutex> lock(mu);
-		// done이 false이면  wait
-		cv.wait(lock, [&] {return done; });
+		// jobq.empty()가 아니고 done이 false이면  wait
+		cv.wait(lock, [&] {return !jobq.empty(); });
 
-		if (ncount == 3)
-			printf("consume =  %d , nCount =  %d\n", nmessage, ncount);
-		lock.unlock();
-		return;
-
-		printf("consume =  %d , nCount =  %d\n", nmessage, ncount);
+		if (done && jobq.empty())
+		{
+			printf("done \nconsume =  %d , nCount =  %d\n", jobq.front(), ncount);
+			lock.unlock();
+			return;
+		}
+		printf("consume =  %d , nCount =  %d\n", jobq.front(), ncount);
+		jobq.pop();
 		lock.unlock();
 		std::this_thread::sleep_for(std::chrono::microseconds(50));
 	}
@@ -58,15 +59,18 @@ void consume() {
 
 int main() {
 
-
+	consumethread.reserve(3);
 	vectorthread.reserve(3);
 	for (int i = 0; i < 3; i++)
 	{
 
-		vectorthread.emplace_back([&]() {work(i); });
+
 		consumethread.emplace_back([]() {consume(); });
+
 	}
 
+	for (int i = 0; i < 3; i++)
+		vectorthread.emplace_back([&]() {work(i); });
 	for (int i = 0; i < 3; i++)
 	{
 		vectorthread[i].join();
