f8d9b43fb5f178cbba74818a8db79cb1dcc1ce08
가테 <79397487+godhye@users.noreply.github.com>
2022-03-18 22:00:09 +0900

    Update receive.cpp

--- file/receive.cpp
+++ file/receive.cpp
@@ -1,42 +1,31 @@
-// echoserver.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+// echoclient.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
 //
 
 #include "stdafx.h"
- 
 #pragma comment (lib , "ws2_32")
-
 
 #include <winsock2.h>
 #include <stdio.h>
 #include <Windows.h>
 #define USE_PORT 4444
 #define BUFSIZE 300
-
+#define ADDR "127.0.0.1"
 int main()
 {
-	
-	//wsa 초기화
+
+	//wsa startup
 	WSADATA wsaData;
+	SOCKADDR_IN sServaddr;
+	SOCKET sServ, sClnt;
 
 	//winsock 동적라이브러리 연결 , 요구사항 충족하는지 확인 
 	//winsock 2.2 버전 요청 
-	int nresult = WSAStartup(MAKEWORD(2,2),&wsaData );
-	if (nresult != 0)
-	{
-		fputs("WSAStartup error", stdin);
-		return -1;
-	}
+	int nresult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	SOCKET hServ, hCnt;
-	SOCKADDR_IN sServaddr, sClntaddr;
-	
-	//socket(), 소켓할당 
-	//PF_INET = ipv4 프로토콜
-	//sock_stream = tcp
-	hServ= socket(PF_INET, SOCK_STREAM, 0);
-	if (hServ == INVALID_SOCKET)
+	//socket
+	sServ = socket(AF_INET, SOCK_STREAM, 0);
+	if (sServ == INVALID_SOCKET)
 	{
-		fputs("WSAStartup error", stdin);
 		puts("hServ error");
 		return -1;
 	}
@@ -45,73 +34,66 @@ int main()
 	memset(&sServaddr, 0, sizeof(SOCKADDR));
 	//AF_INET = ipv4  주소체계 프로토콜
 	sServaddr.sin_family = AF_INET;
-	sServaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	sServaddr.sin_addr.s_addr = inet_addr(ADDR);
 	sServaddr.sin_port = USE_PORT;
-	printf("server ======================= \n");
-	//bind
-	nresult = bind(hServ, (SOCKADDR*)&sServaddr, sizeof(sServaddr));
-	if (nresult != 0)
+
+	//connect
+	nresult = connect(sServ, (SOCKADDR*)&sServaddr, sizeof(sServaddr));
+	if (nresult == SOCKET_ERROR)
 	{
-		fputs("WSAStartup error", stdin);
-		puts("bind error");
+		puts("connect error");
 		return -1;
 	}
+	char file[BUFSIZ] = { 0, };
+	char buf[BUFSIZ * 2] = { 0, };
+	int len = 0;
+	//send
+	printf("client ======================= \n");
+	size_t total = 0;
+	puts("file name :test.mp4 ");
+	sprintf(file, "test.mp4");
 
-	//연결대기 backlog , 연결대기큐갯수 , 모두 찬 상태로 새로운 연결이면 ECONNREPUSED
-	nresult = listen(hServ, 5);
-	if (nresult != 0)
-	{
-		fputs("WSAStartup error", stdin);
-		puts("listen error");
+	FILE *f = fopen(file, "rb");
+	if (!f)
 		return -1;
-	}
-	fputs("listen  ", stdin);
-	int clntsize = sizeof(sClntaddr);
-	for (int i = 0; i < 5; i++)
-	{
+	int nresutl = 0;
 
-	
-		//accept
-		hCnt = accept(hServ, (SOCKADDR*)&sClntaddr, &clntsize);
-		if (hCnt == -1)
+	//파일사이즈 구하기
+	//파일 끝으로 이동
+	fseek(f, 0, SEEK_END);
+	//현재 위치 
+	size_t lsize = ftell(f);
+	//다시 첨으로 이동 
+	rewind(f);
+	while (1)
+	{
+		//fread
+		nresutl = fread((void*)buf, 1, BUFSIZ * 2, f);
+		total += nresutl;
+
+		if (nresutl < BUFSIZ * 2)
 		{
-			fputs("WSAStartup error", stdin);
-			puts("hCnt error");
-			return -1;
-		}
-		printf("connected \n");
-		
-		//read/write
-		int strlen = 0;
-		size_t total = 0;
-		char buf[BUFSIZ*2] = { 0, };
-		FILE *F;
-		F = fopen("test.mp4", "wb");
-		if (!F)
+			int setlen = send(sServ, (char*)buf, nresutl, 0);
+			printf("  client = total = %zd  , fread = %d setlen =%d \n", total, nresutl, setlen);
 			break;
-		//recv
-		while ((strlen = recv(hCnt, buf, BUFSIZ*2, 0)) != 0)
-		{
-			if (strlen == -1)
-			{
-				printf("error \n");
-				fclose(F);
-				break;
-			}
-			total += strlen;
-			 fwrite((void*)F, 1, strlen, F);
 		}
-		fclose(F);
-		//close
-		printf("close  total = %zd \n", total);
-		nresult = closesocket(hCnt);
-		
-	}
-	
-	//소켓정리 
-	closesocket(hServ);
-	//wsa 정리
-	WSACleanup();
-    return 0;
-}
 
+		//send 할때 버퍼사이즈만큼 아니라 fread로 읽은 만큼 보내주기
+		else
+		{
+			nresult = send(sServ, (char*)buf, BUFSIZ * 2, 0);
+			//printf("  client = total = %zd  ,fread = %d \n", total, nresutl);
+		}
+
+	}
+	printf("  client = total = %zd , lsize=%d \n", total, lsize);
+	//파일사이즈만큼 보냈으면 , SHUT_WR
+	shutdown(sServ, SD_SEND);
+
+	//closesocket
+
+	closesocket(sServ);
+	//wsa clean
+	WSACleanup();
+	return 0;
+}
